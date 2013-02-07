c33d3e1fd4084917d6857f08e96894acb99f51b0
Chetan Sarva <chetan@pixelcop.net>
2013-02-07 12:20:07 -0500

    add log to script scope

--- lib/bixby-client/script.rb
+++ lib/bixby-client/script.rb
@@ -1,7 +1,16 @@
 
 require "bixby-client"
+require "fileutils"
+require "systemu"
 
 class Object
   include Bixby::PlatformUtil
   include Bixby::ScriptUtil
+  include Bixby::Log
+
+  # override to create logger based on script name
+  def log
+    @log || = Logging.logger[File.basename($0)]
+  end
+
 end
