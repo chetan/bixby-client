83517a76e09ad68f54e87ab6a1d89a3bc49c4edb
Chetan Sarva <chetan@pixelcop.net>
2013-04-12 15:01:49 -0400

    use hash params

--- lib/bixby-client/modules/inventory.rb
+++ lib/bixby-client/modules/inventory.rb
@@ -4,18 +4,18 @@ module Bixby
 
     # Register an agent with the manager
     #
-    # @param [String] uuid
-    # @param [String] public_key
-    # @param [String] hostname
-    # @param [FixNum] port
-    # @param [String] tenant      Name of the tenant
-    # @param [String] password    Password for registering an Agent with the server
+    # @param [Hash] opts
+    # @option opts [String] :uuid           UUID of the host
+    # @option opts [String] :public_key     Public key
+    # @option opts [String] :hostname       Hostname
+    # @option opts [String] :tenant         Name of the tenant
+    # @option opts [String] :password       Password for registering an Agent with the server
+    # @option opts [FixNum] :port           Port agent listens on (optional, default: 18000)
+    # @option opts [Array<String>] :tags    List of tags to assign to host (optional)
     #
     # @return [JsonResponse]
-    def self.register_agent(uuid, public_key, hostname, port, tenant, password)
-      # TODO these params should probably be a keyed hash instead
-      params = [ uuid, public_key, hostname, port, tenant, password ]
-      req = JsonRequest.new("inventory:register_agent", params)
+    def self.register_agent(opts)
+      req = JsonRequest.new("inventory:register_agent", opts)
       return Bixby::Client.new(nil, nil).exec_api(req)
     end
 
