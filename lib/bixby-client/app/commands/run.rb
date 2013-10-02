dad0b8e655aacd9e9997ab48050fb20679351a45
Chetan Sarva <chetan@pixelcop.net>
2013-10-02 11:04:17 -0400

    split file search to new class

--- lib/bixby-client/app/commands/run.rb
+++ lib/bixby-client/app/commands/run.rb
@@ -1,4 +1,6 @@
 
+require 'bixby-client/app/file_finder'
+
 module Bixby
   module App
     module Commands
@@ -11,7 +13,7 @@ module Bixby
         def run(global_options, argv)
 
           str = argv.shift
-          scripts = find_script(str)
+          scripts = FileFinder.new(Bixby.repo).find_script(str)
 
           if scripts.kind_of?(Array)
             if scripts.size > 1 then
@@ -40,63 +42,6 @@ module Bixby
         def self.options
           nil
         end
-
-        def find_script(script)
-          # try to locate script
-          return script if File.exists? script
-
-          # try relative path
-          root = Bixby.repo
-          s = File.expand_path(File.join(root, script))
-          return s if File.exists? s
-
-          # try searching
-          return find_all_files(root).find_all{ |f| f.include? script }.sort { |a,b|
-            ld(script, File.basename(a)) <=> ld(script, File.basename(b))
-          }
-        end
-
-        def find_all_files(path)
-          ret = []
-          ret += Dir.glob(File.join(path, "**/**")).find_all{ |f|
-            # select only actual script files to look at
-            if File.symlink? f then
-              ret << find_all_files(File.expand_path(f))
-            end
-
-            !File.directory? f and
-              f !~ /\.(json|test.*)|\/digest$/ and
-              File.dirname(f) != path and
-              File.dirname(f) !~ /\/(test|lib)$/
-          }
-
-          return ret.flatten
-        end
-
-        # Calculate the levenshtein distance between two strings
-        #
-        # via https://github.com/akirahrkw/levenshtein-distance
-        # http://en.wikipedia.org/wiki/Levenshtein_distance
-        def levenshtein_distance(a, b)
-          a_len = a.length
-          b_len = b.length
-          d = Array.new(a_len + 1).map! {
-            Array.new(b_len + 1).map!{
-              0
-            }
-          }
-          (a_len + 1).times { |i| d[i][0] = i }
-          (b_len + 1).times { |i| d[0][i] = i }
-
-          for i in 1..(a_len)
-            for j in 1..(b_len)
-              cost = (a[i - 1] == b[j - 1]) ? 0 : 1
-              d[i][j] = [ d[i-1][j] + 1 , d[i][j-1] + 1 ,d[i-1][j-1] + cost].min
-            end
-          end
-          d[-1][-1]
-        end
-        alias_method :ld, :levenshtein_distance
 
       end # Run
       Bixby::Client.app.commands << Run
