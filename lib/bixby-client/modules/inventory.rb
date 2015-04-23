2fb2d646a0b640e85202ab4dea5995d37a472114
Chetan Sarva <chetan@pixelcop.net>
2015-04-23 13:53:02 -0400

    [docs] updated method signature for Inventory#register_agent

--- lib/bixby-client/modules/inventory.rb
+++ lib/bixby-client/modules/inventory.rb
@@ -8,8 +8,7 @@ module Bixby
     # @option opts [String] :uuid           UUID of the host
     # @option opts [String] :public_key     Public key
     # @option opts [String] :hostname       Hostname
-    # @option opts [String] :tenant         Name of the tenant
-    # @option opts [String] :password       Password for registering an Agent with the server
+    # @option opts [String] :token          Client registration token
     # @option opts [FixNum] :port           Port agent listens on (optional, default: 18000)
     # @option opts [Array<String>] :tags    List of tags to assign to host (optional)
     #
